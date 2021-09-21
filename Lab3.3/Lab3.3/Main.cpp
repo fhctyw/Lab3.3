11aba14541d4b5930ba9e3fcda88d8d53bb939e6
fhctyw <lisnychukarsenw@gmail.com>
2021-09-21 22:05:32 +0300

    Write code for lab3.3

--- Lab3.3/Lab3.3/Main.cpp
+++ Lab3.3/Lab3.3/Main.cpp
@@ -12,7 +12,24 @@ int main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
+	double x, R; //Вхідні аргументи
+	double y; //Вихідний аргументи
+	std::cout << "R = "; std::cin >> R;
+	std::cout << "x = "; std::cin >> x;
 
+	if (x <= -R)
+		y = x + R;
+	else
+		if (x > -R && x <= 0)
+			y = sqrt(pow(R, 2) - pow(x, 2));
+		else
+			if (x > 0 && x <= 6)
+				y = R - (R * x) / 6.;
+			else
+				y = x - 6;
+	
+
+	std::cout << "y = " << y << std::endl;
 
 	return 0;
 }
