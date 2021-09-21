5772f90c755b3badb26595e56050f7c624346a92
fhctyw <lisnychukarsenw@gmail.com>
2021-09-21 20:12:42 +0300

    Add empty project

---
+++ Lab3.3/Lab3.3/Main.cpp
@@ -0,0 +1,18 @@
+//	Main.cpp
+//	Лісничук Арсен
+//	Лабораторна робота №3.3
+//	Варіант 13
+
+#include <iostream>
+#include <cmath>
+#include <Windows.h>
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+
+
+	return 0;
+}
